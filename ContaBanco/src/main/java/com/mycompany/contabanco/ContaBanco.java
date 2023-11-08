ece2b876c8e78405b78115b29ce78fb45c9fb0c5
eduardomaurorosa <136762929+eduardomaurorosa@users.noreply.github.com>
2023-11-08 14:00:40 -0300

    Add files via upload

---
+++ ContaBanco/src/main/java/com/mycompany/contabanco/ContaBanco.java
@@ -0,0 +1,30 @@
+
+package com.mycompany.contabanco;
+
+import java.util.Scanner;
+/**
+ *
+ * @author Eduardo Mauro Rosa
+ */
+public class ContaBanco {
+
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta: ");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); 
+        System.out.println("Por favor, digite o nome do cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
+    }
+}
